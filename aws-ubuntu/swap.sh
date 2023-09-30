785aed0d1d5409d618386414519423a2439a5db6
Md Ariful Islam <101464076+matrixkloud@users.noreply.github.com>
2023-09-30 16:19:28 +0600

    Update swap.sh

--- aws-ubuntu/swap.sh
+++ aws-ubuntu/swap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define the swapfile name as a variable (you can change it here if needed)
+SWAPFILE="/swapfile"
+
 # Get the total RAM in kilobytes
 TOTAL_RAM_KB=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 
@@ -8,28 +11,35 @@ TOTAL_RAM_KB=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 SWAP_SIZE_MB=$((TOTAL_RAM_KB / 1024 * 2))
 
 # Check if the swap file already exists
-if [ -f /swapfile ]; then
-    echo "Swap file already exists."
-    exit 1
+if [ -f "$SWAPFILE" ]; then
+    # Get the current swap size in MB
+    CURRENT_SWAP_SIZE_MB=$(du -m "$SWAPFILE" | cut -f1)
+
+    if [ "$CURRENT_SWAP_SIZE_MB" -eq "$SWAP_SIZE_MB" ]; then
+        echo "Swap file already exists and is the correct size."
+        exit 1
+    else
+        # Delete the existing swap file
+        sudo swapoff "$SWAPFILE"
+        sudo rm "$SWAPFILE"
+        echo "Existing swap file deleted."
+    fi
 fi
 
-# Create a swap file with the calculated size
-sudo fallocate -l ${SWAP_SIZE_MB}M /swapfile
+# Create a new swap file with the calculated size
+sudo fallocate -l ${SWAP_SIZE_MB}M "$SWAPFILE"
 
 # Secure the swap file by restricting access
-sudo chmod 600 /swapfile
+sudo chmod 600 "$SWAPFILE"
 
 # Set up the swap area
-sudo mkswap /swapfile
+sudo mkswap "$SWAPFILE"
 
 # Enable the swap file
-sudo swapon /swapfile
-
-# Define the swapfile path
-swapfile="/swapfile"
+sudo swapon "$SWAPFILE"
 
 # Define the line to add to /etc/fstab
-line_to_add="/swapfile none swap sw 0 0"
+line_to_add="$SWAPFILE none swap sw 0 0"
 
 # Check if the line already exists in /etc/fstab
 if grep -qFx "$line_to_add" /etc/fstab; then
@@ -47,3 +57,4 @@ sudo swapon --show
 free -h
 
 echo "Swap memory of ${SWAP_SIZE_MB}MB has been added."
+
